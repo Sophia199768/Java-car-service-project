058b9148348b824a1d1e35f1a8405bf30bcdeb93
sophia <sofiya.brovkina@inbox.ru>
2024-08-10 15:18:29 +0300

    add homework2

--- src/main/java/org/example/in/parser/Parser.java
+++ src/main/java/org/example/in/parser/Parser.java
@@ -3,6 +3,7 @@ package org.example.in.parser;
 import org.example.application.ApplicationState;
 import org.example.in.parser.command.Commandable;
 
+
 public interface Parser {
     Commandable parse(String input, ApplicationState state);
 }
